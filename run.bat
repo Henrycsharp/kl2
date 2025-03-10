c8c3347b25f62538824752ee360d0a74ff032df3
henrycsharp <151407721+Henrycsharp@users.noreply.github.com>
2025-03-10 19:00:53 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -10,7 +10,7 @@ pip install pynput
 call autostart.bat
 
 echo Running main.py...
-pythonw main.py
+wscript run_invisible.vbs
 if %errorlevel% neq 0 (
     echo Error occurred while running main.py.
     pause
