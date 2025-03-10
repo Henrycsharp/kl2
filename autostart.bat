fcf317f54faedb986fe5b9b1981a8e2e3526d773
henrycsharp <151407721+Henrycsharp@users.noreply.github.com>
2025-03-10 17:33:07 +0100

    Update autostart.bat

--- autostart.bat
+++ autostart.bat
@@ -1,11 +1,13 @@
 @echo off
 set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-set "batFilePath=C:\path\to\your\run.bat"
+set "batFilePath=C:\users\%USERNAME%\kl2\run.bat"
 
 :: Check if the batch file already exists in the Startup folder
-if not exist "C:\Users\%USERNAME%\kl2\run.bat" (
-    copy "%batFilePath%" "%startupFolder%t"
+if not exist "%startupFolder%\run.bat" (
+    copy "%batFilePath%" "%startupFolder%"
     echo run.bat has been added to the Startup folder.
 ) else (
     echo run.bat is already in the Startup folder.
 )
+
+exit
